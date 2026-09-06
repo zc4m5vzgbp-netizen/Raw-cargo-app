1ff1b44a4f58bc7fdfdc5de9a3d5d268ee235fe7
zc4m5vzgbp-netizen <zc4m5vzgbp@privaterelay.appleid.com>
2026-09-06 08:20:25 -0500

    Update finanzas.js

--- js/modules/finanzas.js
+++ js/modules/finanzas.js
@@ -1,2 +1,3 @@
 export async function render(contenedor) {
-  contenedor.innerHTML = '<h2>Finanzas</h2><p style="color:var(--color-texto-secundario)">Módulo en construcción — próxima fase.</p>'
+  contenedor.innerHTML = '<h2>Finanzas</h2><p style="color:var(--color-texto-secundario)">Módulo en construcción — próxima fase.</p>';
+}
